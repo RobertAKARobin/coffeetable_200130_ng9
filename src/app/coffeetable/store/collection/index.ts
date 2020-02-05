da5d12153f895dce5e7ade78148b41c73650e6af
RobertAKARobin <contact@robertakarobin.com>
2020-02-05 08:48:35 -0600

    rm old comments

--- src/app/coffeetable/store/collection/index.ts
+++ src/app/coffeetable/store/collection/index.ts
@@ -1,32 +1,3 @@
-// export type functionString = string;
-
-// export type DisplayValue = (
-//   string
-//   | functionString
-//   | number
-//   | undefined
-//   | boolean
-//   | null
-//   | void
-// );
-
-// export interface Record {
-//   data: any;
-// }
-
-// export interface Field {
-//   getter: functionString;
-//   name: string;
-//   setter: functionString;
-// }
-
-// export interface Collection {
-//   fields: Field[];
-//   filterOn: functionString;
-//   orderOn: functionString;
-//   records: Record[];
-// }
-
 import * as Actions from './actions';
 import * as Store from './reducers';
 
